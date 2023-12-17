3bd44cc700561be614351cb4c187b154bb6760e8
渡邉広之 <watanabehiroyuki@watanabehiroyukinonotobukkukonpyuta.local>
2023-12-17 23:31:59 +0900

    FizzBuzz

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :pages do
+    collection do
+      get 'fizzbuzz'
+      get 'click_count'
+    end
+  end
+  root to: 'pages#index'
 end
